c4d995161c45983b4a866dc18e5c80293d693cb6
syed samad ali <samadali0125@gmail.com>
2025-05-24 19:27:08 +0530

    add Backend Uri

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      "/api":"http://localhost:5000"
-    }
+    // proxy:{
+    //   "/api":"http://localhost:5000"
+    // }
   },
   plugins: [react(), tailwindcss()],
 })
