f2be50489c589045c9f29719040cd4d44a9462eb
syed samad ali <samadali0125@gmail.com>
2025-05-25 14:39:37 +0530

    add frontend

--- src/services/Notes.services.jsx
+++ src/services/Notes.services.jsx
@@ -2,11 +2,11 @@ import axiosInstance from "../utils/AxiosInstance";
 
 
 // NOTES APIs
-export const getAllNotes = () => axiosInstance.get('/api/notes/all');
-export const createNote = (data) => axiosInstance.post('/api/notes/create', data);
-export const updateNote = (payload) => axiosInstance.put('/api/notes/update', payload);
-export const getUserNotes = () => axiosInstance.get('/api/notes/user');
+export const getAllNotes = () => axiosInstance.get('/notes/all');
+export const createNote = (data) => axiosInstance.post('/notes/create', data);
+export const updateNote = (payload) => axiosInstance.put('/notes/update', payload);
+export const getUserNotes = () => axiosInstance.get('/notes/user');
 export const searchNotes = (query) =>
   axiosInstance.get(`/api/notes/search?${query.join('&')}`);
 export const getNotesByCategory = (category) =>
-  axiosInstance.get(`/api/notes/category?category=${encodeURIComponent(category)}`);
+  axiosInstance.get(`/notes/category?category=${encodeURIComponent(category)}`);
--- src/services/User.services.jsx
+++ src/services/User.services.jsx
@@ -1,6 +1,6 @@
 import axiosInstance from "../utils/AxiosInstance";
 
 // AUTH APIs
-export const registerUser = (data) => axiosInstance.post('/api/auth/register', data);
-export const loginUser = (data) => axiosInstance.post('/api/auth/login', data);
-export const logoutUser = () => axiosInstance.get('/api/auth/logout');
+export const registerUser = (data) => axiosInstance.post('/auth/register', data);
+export const loginUser = (data) => axiosInstance.post('/auth/login', data);
+export const logoutUser = () => axiosInstance.get('/auth/logout');
